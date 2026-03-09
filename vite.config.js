a2351e37985e6ce92c494b0caab2af815372c573
Fredrik Andersson <freand01@gafe.molndal.se>
2026-03-09 10:42:11 +0100

    Lade till rums i namnet

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Byt ut namnet nedan till EXAKT det namn du gav ditt repository på GitHub
 export default defineConfig({
-  base: '/Klassplacering-v2/', 
+  base: '/Klassrumsplacering-v2/', 
   plugins: [react()],
 })
